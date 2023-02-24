9f750c1c3cdb9dcca8d4fb2d0951840aa24a324d
zhouyw16 <zhouyw1216@163.com>
2023-02-24 23:48:34 +0800

    imagenet

--- curbench/datasets/vision/imagenet32.py
+++ curbench/datasets/vision/imagenet32.py
@@ -1,88 +1,115 @@
+'''
+Based on https://pytorch.org/vision/stable/_modules/torchvision/datasets/cifar.html#CIFAR10
+'''
 
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
-import tensorflow_datasets as tfds
-from torch.utils.data import Dataset
-from torchvision import transforms
+import os.path
+import pickle
+from typing import Any, Callable, Optional, Tuple
 
-from .utils import Cutout, LabelNoise
+import numpy as np
+from PIL import Image
 
 
-class ImageNet32(Dataset):
-    def __init__(self, dataset, transform=None):
-        self.dataset = dataset
+class ImageNet32():
+    """`ImageNet 32x32 <https://patrykchrabaszcz.github.io/Imagenet32/>`_ Dataset.
+
+    Args:
+        root (string): Root directory of dataset where directory
+            ``imagenet32`` exists or will be saved to if download is set to True.
+        train (bool, optional): If True, creates dataset from training set, otherwise
+            creates from test set.
+        transform (callable, optional): A function/transform that takes in an PIL image
+            and returns a transformed version. E.g, ``transforms.RandomCrop``
+        target_transform (callable, optional): A function/transform that takes in the
+            target and transforms it.
+    """
+    def __init__(
+        self,
+        root: str,
+        train: bool = True,
+        transform: Optional[Callable] = None,
+        target_transform: Optional[Callable] = None,
+    ) -> None:
+
+        self.root = root
+        self.train = train  # training set or test set
         self.transform = transform
+        self.target_transform = target_transform
 
-    def __getitem__(self, index):
-        for image, label in self.dataset: 
-            # batch_size = 1
-            # image = [batch_size, height, weight, channel]
-            # label = [batch_size, 1]
-            if self.transform: image = self.transform(image[0])
-            label = label[0]
-            yield image, label
+        if self.train:
+            downloaded_list = ['train_data_batch_%d' % (i) for i in range(1, 11)]
+        else:
+            downloaded_list = ['val_data']
 
-    def __len__(self):
-        return len(self.dataset)
+        self.data: Any = []
+        self.targets = []
 
+        # now load the picked numpy arrays
+        for file_name in downloaded_list:
+            file_path = os.path.join(self.root, 'imagenet32', file_name)
+            with open(file_path, "rb") as f:
+                entry = pickle.load(f, encoding="latin1")
+                self.data.append(entry["data"])
+                self.targets.extend(entry["labels"])
 
-def get_imagenet32_dataset(data_dir='data', valid_ratio=0.1, 
-                           augment=True, cutout_length=0, noise_ratio=0.0):
-    assert ((valid_ratio >= 0) and (valid_ratio <= 1)), \
-        'Assert Error: valid_size should be in the range [0, 1].'
-    valid_percent = int(valid_ratio * 100)
+        self.data = np.vstack(self.data).reshape(-1, 3, 32, 32)
+        self.data = self.data.transpose((0, 2, 3, 1))  # convert to HWC
 
-    MEAN = [0.485, 0.456, 0.406]
-    STD = [0.229, 0.224, 0.225]
+    def __getitem__(self, index: int) -> Tuple[Any, Any]:
+        """
+        Args:
+            index (int): Index
 
-    train_dataset = tfds.as_numpy(tfds.load('imagenet_resized/32x32', 
-        split='train[%d%%:]' % (valid_percent), data_dir=data_dir, 
-        batch_size=1, shuffle_files=True, download=False, as_supervised=True)
-    )
-    valid_dataset, test_dataset = tfds.as_numpy(tfds.load('imagenet_resized/32x32', 
-        split=['train[:%d%%]' % (valid_percent), 'validation'], data_dir=data_dir, 
-        batch_size=1, shuffle_files=False, download=False, as_supervised=True)
-    )
-    
-    transf = [
-        transforms.ToPILImage(),
-        transforms.RandomCrop(32, padding=4),
-        transforms.RandomHorizontalFlip(),
-    ] if augment else []
-    normalize = [
-        transforms.ToTensor(),
-        transforms.Normalize(MEAN, STD),
-    ]
-    cutout = [Cutout(cutout_length)] \
-      if cutout_length > 0 else []
-    
-    train_transform = transforms.Compose(transf + normalize + cutout)
-    test_transform = transforms.Compose(normalize)
+        Returns:
+            tuple: (image, target) where target is index of the target class.
+        """
+        img, target = self.data[index], self.targets[index]
 
-    train_dataset = ImageNet32(
-        dataset=train_dataset, transform=train_transform,
-    )
-    valid_dataset = ImageNet32(
-        dataset=valid_dataset, transform=test_transform,
-    )
-    test_dataset = ImageNet32(
-        dataset=test_dataset, transform=test_transform,
-    )
+        # doing this so that it is consistent with all other datasets
+        # to return a PIL Image
+        img = Image.fromarray(img)
 
-    if noise_ratio > 0.0:
-        train_dataset = LabelNoise(train_dataset, noise_ratio, 1000)
+        if self.transform is not None:
+            img = self.transform(img)
 
-    return train_dataset, valid_dataset, test_dataset
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    def extra_repr(self) -> str:
+        split = "Train" if self.train is True else "Test"
+        return f"Split: {split}"
 
 
 if __name__ == '__main__':
-    train_dataset = tfds.as_numpy(tfds.load('imagenet_resized/32x32', 
-        split='train[%d%%:]' % (0.1), data_dir='data', 
-        batch_size=1, shuffle_files=True, download=False, as_supervised=True)
-    )
-    transform = transforms.ToPILImage()
-    for image, label in train_dataset:
-        print(image[0])
-        print(label[0])
-        print(transform(image[0]))
-        input()
+    import torch
+    from torch.utils.data import DataLoader
+    import torchvision
+    import torchvision.transforms as transforms
+
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    trainset = ImageNet32('data', train=True, transform=transform)
+    testset = ImageNet32('data', train=False, transform=transform)
+    trainloader = DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
+    testloader = DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
+    print(len(trainset), len(testset))
+
+    import matplotlib.pyplot as plt
+    def imshow(img):
+        img = img / 2 + 0.5     # unnormalize
+        npimg = img.numpy()
+        fig = np.transpose(npimg, (1, 2, 0))
+        plt.imshow(fig)
+        plt.savefig('img.png')
+
+    dataiter = iter(testloader)
+    images, labels = next(dataiter)
+    imshow(torchvision.utils.make_grid(images))
