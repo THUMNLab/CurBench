7663337bc855530c57ca4723f56ffbc44d88d98d
zhouyw16 <zhouyw1216@163.com>
2024-01-28 14:08:15 +0000

    fix gat conv

--- curbench/backbones/graph/gnn_ogb.py
+++ curbench/backbones/graph/gnn_ogb.py
@@ -1,42 +1,19 @@
 import torch
 import torch.nn.functional as F
-from torch_geometric.nn import MessagePassing, GATConv
-from torch_geometric.nn import global_add_pool, global_mean_pool, global_max_pool, GlobalAttention, Set2Set
+from torch_geometric.nn import MessagePassing, GATConv, GlobalAttention, Set2Set, \
+                               global_add_pool, global_mean_pool, global_max_pool
 from torch_geometric.utils import degree, add_self_loops, remove_self_loops, softmax
-from ogb.graphproppred.mol_encoder import AtomEncoder,BondEncoder
-
-from typing import Optional, Union
-from torch import Tensor
-from torch.nn import Parameter
-
-from torch_geometric.nn.dense.linear import Linear
-from torch_geometric.nn.inits import glorot, zeros
-from torch_geometric.typing import NoneType  # noqa
-from torch_geometric.typing import (
-    Adj,
-    OptPairTensor,
-    OptTensor,
-    Size,
-    SparseTensor,
-    torch_sparse,
-)
-from torch_geometric.utils import (
-    is_torch_sparse_tensor,
-)
-from torch_geometric.utils.sparse import set_sparse_value
-
-from torch_sparse import SparseTensor, set_diag
-
+from ogb.graphproppred.mol_encoder import AtomEncoder, BondEncoder
 
 
 ### GIN convolution along the graph structure
-class GINConv(MessagePassing):
+class GINConvOGB(MessagePassing):
     def __init__(self, emb_dim):
         '''
             emb_dim (int): node embedding dimensionality
         '''
 
-        super(GINConv, self).__init__(aggr = "add")
+        super(GINConvOGB, self).__init__(aggr = "add")
 
         self.mlp = torch.nn.Sequential(torch.nn.Linear(emb_dim, 2*emb_dim), torch.nn.BatchNorm1d(2*emb_dim), torch.nn.ReLU(), torch.nn.Linear(2*emb_dim, emb_dim))
         self.eps = torch.nn.Parameter(torch.Tensor([0]))
@@ -56,154 +33,55 @@ class GINConv(MessagePassing):
         return aggr_out
 
 
-class GATConvMol(MessagePassing):
-    def __init__(
-        self,
-        in_channels: int = 300,
-        out_channels: int = 300,
-        heads: int = 1,
-        concat: bool = True,
-        negative_slope: float = 0.2,
-        dropout: float = 0.0,
-        add_self_loops: bool = True,
-        edge_dim: Optional[int] = None,
-        fill_value: Union[float, Tensor, str] = 'mean',
-        bias: bool = True,
-        **kwargs,
-    ):
-        kwargs.setdefault('aggr', 'add')
-        super().__init__(node_dim=0, **kwargs)
+### GAT convolution along the graph structure
+class GATConvOGB(GATConv):
+    def __init__(self, in_dim, out_dim):
+        super().__init__(in_dim,out_dim, add_self_loops = False)
+        self.bond_encoder = BondEncoder(emb_dim = out_dim)
 
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.heads = heads
-        self.concat = concat
-        self.negative_slope = negative_slope
-        self.dropout = dropout
-        self.add_self_loops = add_self_loops
-        self.edge_dim = edge_dim
-        self.fill_value = fill_value
-
-        # In case we are operating in bipartite graphs, we apply separate
-        # transformations 'lin_src' and 'lin_dst' to source and target nodes:
-        if isinstance(in_channels, int):
-            self.lin_src = Linear(in_channels, heads * out_channels,
-                                  bias=False, weight_initializer='glorot')
-            self.lin_dst = self.lin_src
-        else:
-            self.lin_src = Linear(in_channels[0], heads * out_channels, False,
-                                  weight_initializer='glorot')
-            self.lin_dst = Linear(in_channels[1], heads * out_channels, False,
-                                  weight_initializer='glorot')
-
-        self.bond_encoder = BondEncoder(heads * out_channels)
-
-        # The learnable parameters to compute attention coefficients:
-        self.att_src = Parameter(torch.empty(1, heads, out_channels))
-        self.att_dst = Parameter(torch.empty(1, heads, out_channels))
-
-        if edge_dim is not None:
-            self.lin_edge = Linear(edge_dim, heads * out_channels, bias=False,
-                                   weight_initializer='glorot')
-            self.att_edge = Parameter(torch.empty(1, heads, out_channels))
-        else:
-            self.lin_edge = None
-            self.register_parameter('att_edge', None)
-
-        if bias and concat:
-            self.bias = Parameter(torch.empty(heads * out_channels))
-        elif bias and not concat:
-            self.bias = Parameter(torch.empty(out_channels))
-        else:
-            self.register_parameter('bias', None)
-
-        self.reset_parameters()
-
-    def reset_parameters(self):
-        super().reset_parameters()
-        self.lin_src.reset_parameters()
-        self.lin_dst.reset_parameters()
-        if self.lin_edge is not None:
-            self.lin_edge.reset_parameters()
-        glorot(self.att_src)
-        glorot(self.att_dst)
-        glorot(self.att_edge)
-        zeros(self.bias)
-
-    def forward(self, x: Union[Tensor, OptPairTensor], edge_index: Adj,
-                edge_attr: OptTensor = None, size: Size = None,
-                return_attention_weights=None):
-        # type: (Union[Tensor, OptPairTensor], Tensor, OptTensor, Size, NoneType) -> Tensor  # noqa
-        # type: (Union[Tensor, OptPairTensor], SparseTensor, OptTensor, Size, NoneType) -> Tensor  # noqa
-        # type: (Union[Tensor, OptPairTensor], Tensor, OptTensor, Size, bool) -> Tuple[Tensor, Tuple[Tensor, Tensor]]  # noqa
-        # type: (Union[Tensor, OptPairTensor], SparseTensor, OptTensor, Size, bool) -> Tuple[Tensor, SparseTensor]  # noqa
-        r"""Runs the forward pass of the module.
-
-        Args:
-            return_attention_weights (bool, optional): If set to :obj:`True`,
-                will additionally return the tuple
-                :obj:`(edge_index, attention_weights)`, holding the computed
-                attention weights for each edge. (default: :obj:`None`)
-        """
-        # NOTE: attention weights will be returned whenever
-        # `return_attention_weights` is set to a value, regardless of its
-        # actual value (might be `True` or `False`). This is a current somewhat
-        # hacky workaround to allow for TorchScript support via the
-        # `torch.jit._overload` decorator, as we can only change the output
-        # arguments conditioned on type (`None` or `bool`), not based on its
-        # actual value.
-
+    def forward(self, x, edge_index, edge_attr, 
+                size = None, return_attention_weights=None):
+        edge_embedding = self.bond_encoder(edge_attr)
         H, C = self.heads, self.out_channels
 
-        # We first transform the input node features. If a tuple is passed, we
-        # transform source and target node features via separate weights:
-        if isinstance(x, Tensor):
-            assert x.dim() == 2, "Static graphs not supported in 'GATConv'"
+        x_src = None
+        x_dst = None
+        alpha_src = None
+        alpha_dst = None
+        if isinstance(x, torch.Tensor):
+            assert x.dim() == 2, 'Static graphs not supported in `GATConv`.'
             x_src = x_dst = self.lin_src(x).view(-1, H, C)
-        else:  # Tuple of source and target node features:
-            x_src, x_dst = x
-            assert x_src.dim() == 2, "Static graphs not supported in 'GATConv'"
+            alpha_src = (x_src * self.att_src).sum(dim=-1)
+            alpha_dst = (x_dst * self.att_dst).sum(dim=-1)
+        else:
+            x_src, x_dst = x[0], x[1]
+            assert x[0].dim() == 2, 'Static graphs not supported in `GATConv`.'
             x_src = self.lin_src(x_src).view(-1, H, C)
+            alpha_src = (x_src * self.att_src).sum(dim=-1)
             if x_dst is not None:
                 x_dst = self.lin_dst(x_dst).view(-1, H, C)
+                alpha_dst = (x_dst * self.att_dst).sum(dim=-1)
 
-        x = (x_src, x_dst)
-
-        # Next, we compute node-level attention coefficients, both for source
-        # and target nodes (if present):
-        alpha_src = (x_src * self.att_src).sum(dim=-1)
-        alpha_dst = None if x_dst is None else (x_dst * self.att_dst).sum(-1)
-        alpha = (alpha_src, alpha_dst)
+        assert x_src is not None
+        assert alpha_src is not None
 
         if self.add_self_loops:
-            if isinstance(edge_index, Tensor):
-                # We only want to add self-loops for nodes that appear both as
-                # source and target nodes:
+            if isinstance(edge_index, torch.Tensor):
                 num_nodes = x_src.size(0)
                 if x_dst is not None:
                     num_nodes = min(num_nodes, x_dst.size(0))
-                num_nodes = min(size) if size is not None else num_nodes
-                edge_index, edge_attr = remove_self_loops(
-                    edge_index, edge_attr)
-                edge_index, edge_attr = add_self_loops(
-                    edge_index, edge_attr, fill_value=self.fill_value,
-                    num_nodes=num_nodes)
-            elif isinstance(edge_index, SparseTensor):
-                if self.edge_dim is None:
-                    edge_index = torch_sparse.set_diag(edge_index)
-                else:
-                    raise NotImplementedError(
-                        "The usage of 'edge_attr' and 'add_self_loops' "
-                        "simultaneously is currently not yet supported for "
-                        "'edge_index' in a 'SparseTensor' form")
+                if size is not None:
+                    num_nodes = min(size[0], size[1])
+                edge_index, _ = remove_self_loops(edge_index)
+                edge_index, _ = add_self_loops(edge_index, num_nodes=num_nodes)
+            # elif isinstance(edge_index, SparseTensor):
+            #     edge_index = set_diag(edge_index)
 
-        # edge_updater_type: (alpha: OptPairTensor, edge_attr: OptTensor)
-        alpha = self.edge_updater(edge_index, alpha=alpha, edge_attr=edge_attr)
+        out = self.propagate(edge_index, x=(x_src, x_dst),
+                             alpha=(alpha_src, alpha_dst), size=size, edge_attr = edge_embedding)
 
-        edge_embedding = self.bond_encoder(edge_attr.long())
-
-        # propagate_type: (x: OptPairTensor, alpha: Tensor)
-        out = self.propagate(edge_index, x=x, alpha=alpha, size=size, edge_attr=edge_embedding)
+        alpha = self._alpha
+        self._alpha = None
 
         if self.concat:
             out = out.view(-1, self.heads * self.out_channels)
@@ -211,148 +89,33 @@ class GATConvMol(MessagePassing):
             out = out.mean(dim=1)
 
         if self.bias is not None:
-            out = out + self.bias
+            out += self.bias
 
         if isinstance(return_attention_weights, bool):
-            if isinstance(edge_index, Tensor):
-                if is_torch_sparse_tensor(edge_index):
-                    # TODO TorchScript requires to return a tuple
-                    adj = set_sparse_value(edge_index, alpha)
-                    return out, (adj, alpha)
-                else:
-                    return out, (edge_index, alpha)
-            elif isinstance(edge_index, SparseTensor):
-                return out, edge_index.set_value(alpha, layout='coo')
+            assert alpha is not None
+            if isinstance(edge_index, torch.Tensor):
+                return out, (edge_index, alpha)
+            # elif isinstance(edge_index, SparseTensor):
+            #     return out, edge_index.set_value(alpha, layout='coo')
         else:
             return out
 
-    def edge_update(self, alpha_j: Tensor, alpha_i: OptTensor,
-                    edge_attr: OptTensor, index: Tensor, ptr: OptTensor,
-                    size_i: Optional[int]) -> Tensor:
-        # Given edge-level attention coefficients for source and target nodes,
-        # we simply need to sum them up to "emulate" concatenation:
-        alpha = alpha_j if alpha_i is None else alpha_j + alpha_i
-        if index.numel() == 0:
-            return alpha
-        if edge_attr is not None and self.lin_edge is not None:
-            if edge_attr.dim() == 1:
-                edge_attr = edge_attr.view(-1, 1)
-            edge_attr = self.lin_edge(edge_attr)
-            edge_attr = edge_attr.view(-1, self.heads, self.out_channels)
-            alpha_edge = (edge_attr * self.att_edge).sum(dim=-1)
-            alpha = alpha + alpha_edge
+    def main_para(self):
+        return [self.lin_src.weight]
 
+    def message(self, x_j, alpha_j, alpha_i, index, ptr, size_i, edge_attr):
+        alpha = alpha_j if alpha_i is None else alpha_j + alpha_i
         alpha = F.leaky_relu(alpha, self.negative_slope)
         alpha = softmax(alpha, index, ptr, size_i)
+        self._alpha = alpha
         alpha = F.dropout(alpha, p=self.dropout, training=self.training)
-        return alpha
+        return F.relu(x_j + edge_attr.unsqueeze(1)) * alpha.unsqueeze(-1)
 
-    def message(self, x_j: Tensor, alpha: Tensor, edge_attr: Tensor) -> Tensor:
-        return alpha.unsqueeze(-1) * F.relu(x_j + edge_attr.view(-1, self.heads, self.out_channels))
-
-    def update(self, aggr_out):
-        return aggr_out
-
-    def __repr__(self) -> str:
-        return (f'{self.__class__.__name__}({self.in_channels}, '
-                f'{self.out_channels}, heads={self.heads})')
-
-
-# class GATConvMol(GATConv):
-#     def __init__(self, in_dim, out_dim):
-#         super().__init__(in_dim,out_dim, add_self_loops = False)
-#         self.bond_encoder = BondEncoder(emb_dim = out_dim)
-
-#     def forward(self, x: Union[Tensor, OptPairTensor], edge_index: Adj, edge_attr, 
-#                 size: Size = None, return_attention_weights=None):
-#         # type: (Union[Tensor, OptPairTensor], Tensor, Size, NoneType) -> Tensor  # noqa
-#         # type: (Union[Tensor, OptPairTensor], SparseTensor, Size, NoneType) -> Tensor  # noqa
-#         # type: (Union[Tensor, OptPairTensor], Tensor, Size, bool) -> Tuple[Tensor, Tuple[Tensor, Tensor]]  # noqa
-#         # type: (Union[Tensor, OptPairTensor], SparseTensor, Size, bool) -> Tuple[Tensor, SparseTensor]  # noqa
-#         r"""
-#         Args:
-#             return_attention_weights (bool, optional): If set to :obj:`True`,
-#                 will additionally return the tuple
-#                 :obj:`(edge_index, attention_weights)`, holding the computed
-#                 attention weights for each edge. (default: :obj:`None`)
-#         """
-#         edge_embedding = self.bond_encoder(edge_attr)
-#         H, C = self.heads, self.out_channels
-
-#         x_src: OptTensor = None
-#         x_dst: OptTensor = None
-#         alpha_src: OptTensor = None
-#         alpha_dst: OptTensor = None
-#         if isinstance(x, Tensor):
-#             assert x.dim() == 2, 'Static graphs not supported in `GATConv`.'
-#             x_src = x_dst = self.lin_src(x).view(-1, H, C)
-#             alpha_src = (x_src * self.att_src).sum(dim=-1)
-#             alpha_dst = (x_dst * self.att_dst).sum(dim=-1)
-#         else:
-#             x_src, x_dst = x[0], x[1]
-#             assert x[0].dim() == 2, 'Static graphs not supported in `GATConv`.'
-#             x_src = self.lin_src(x_src).view(-1, H, C)
-#             alpha_src = (x_src * self.att_src).sum(dim=-1)
-#             if x_dst is not None:
-#                 x_dst = self.lin_dst(x_dst).view(-1, H, C)
-#                 alpha_dst = (x_dst * self.att_dst).sum(dim=-1)
-
-#         assert x_src is not None
-#         assert alpha_src is not None
-
-#         if self.add_self_loops:
-#             if isinstance(edge_index, Tensor):
-#                 num_nodes = x_src.size(0)
-#                 if x_dst is not None:
-#                     num_nodes = min(num_nodes, x_dst.size(0))
-#                 if size is not None:
-#                     num_nodes = min(size[0], size[1])
-#                 edge_index, _ = remove_self_loops(edge_index)
-#                 edge_index, _ = add_self_loops(edge_index, num_nodes=num_nodes)
-#             elif isinstance(edge_index, SparseTensor):
-#                 edge_index = set_diag(edge_index)
-
-#         # propagate_type: (x: OptPairTensor, alpha: OptPairTensor)
-#         out = self.propagate(edge_index, x=(x_src, x_dst),
-#                              alpha=(alpha_src, alpha_dst), size=size, edge_attr = edge_embedding)
-
-#         alpha = self._alpha
-#         self._alpha = None
-
-#         if self.concat:
-#             out = out.view(-1, self.heads * self.out_channels)
-#         else:
-#             out = out.mean(dim=1)
-
-#         if self.bias is not None:
-#             out += self.bias
-
-#         if isinstance(return_attention_weights, bool):
-#             assert alpha is not None
-#             if isinstance(edge_index, Tensor):
-#                 return out, (edge_index, alpha)
-#             elif isinstance(edge_index, SparseTensor):
-#                 return out, edge_index.set_value(alpha, layout='coo')
-#         else:
-#             return out
-
-#     def main_para(self):
-#         return [self.lin_src.weight]
-
-#     def message(self, x_j: Tensor, alpha_j: Tensor, alpha_i: OptTensor,
-#                 index: Tensor, ptr: OptTensor,
-#                 size_i: Optional[int], edge_attr) -> Tensor:
-#         alpha = alpha_j if alpha_i is None else alpha_j + alpha_i
-#         alpha = F.leaky_relu(alpha, self.negative_slope)
-#         alpha = softmax(alpha, index, ptr, size_i)
-#         self._alpha = alpha
-#         alpha = F.dropout(alpha, p=self.dropout, training=self.training)
-#         return F.relu(x_j + edge_attr.unsqueeze(1)) * alpha.unsqueeze(-1)
 
 ### GCN convolution along the graph structure
-class GCNConv(MessagePassing):
+class GCNConvOGB(MessagePassing):
     def __init__(self, emb_dim):
-        super(GCNConv, self).__init__(aggr='add')
+        super(GCNConvOGB, self).__init__(aggr='add')
 
         self.linear = torch.nn.Linear(emb_dim, emb_dim)
         self.root_emb = torch.nn.Embedding(1, emb_dim)
@@ -381,7 +144,7 @@ class GCNConv(MessagePassing):
 
 
 ### GNN to generate node embedding
-class GNN_node(torch.nn.Module):
+class GNNNodeOGB(torch.nn.Module):
     """
     Output:
         node representations
@@ -393,7 +156,7 @@ class GNN_node(torch.nn.Module):
 
         '''
 
-        super(GNN_node, self).__init__()
+        super(GNNNodeOGB, self).__init__()
         self.num_layer = num_layer
         self.drop_ratio = drop_ratio
         self.JK = JK
@@ -411,11 +174,11 @@ class GNN_node(torch.nn.Module):
 
         for layer in range(num_layer):
             if gnn_type == 'gin':
-                self.convs.append(GINConv(emb_dim))
+                self.convs.append(GINConvOGB(emb_dim))
             elif gnn_type == 'gat':
-                self.convs.append(GATConvMol(emb_dim, emb_dim))
+                self.convs.append(GATConvOGB(emb_dim, emb_dim))
             elif gnn_type == 'gcn':
-                self.convs.append(GCNConv(emb_dim))
+                self.convs.append(GCNConvOGB(emb_dim))
             else:
                 raise ValueError('Undefined GNN type called {}'.format(gnn_type))
 
@@ -476,7 +239,7 @@ class GNNForOGB(torch.nn.Module):
             raise ValueError("Number of GNN layers must be greater than 1.")
 
         ### GNN to generate node embeddings
-        self.gnn_node = GNN_node(num_layer, emb_dim, JK = JK, drop_ratio = drop_ratio, residual = residual, gnn_type = gnn_type)
+        self.gnn_node = GNNNodeOGB(num_layer, emb_dim, JK = JK, drop_ratio = drop_ratio, residual = residual, gnn_type = gnn_type)
 
         ### Pooling function to generate whole-graph embeddings
         if self.graph_pooling == "sum":
